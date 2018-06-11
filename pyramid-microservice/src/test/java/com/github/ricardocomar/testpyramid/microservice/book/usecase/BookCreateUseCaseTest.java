3173401d740612db418ef7412b37c47e58e5c8ea
Marcelo Miranda <mycelo19@gmail.com>
2018-06-11 19:31:02 -0300

    estrutura

--- pyramid-microservice/src/test/java/com/github/ricardocomar/testpyramid/microservice/book/usecase/BookCreateUseCaseTest.java
+++ pyramid-microservice/src/test/java/com/github/ricardocomar/testpyramid/microservice/book/usecase/BookCreateUseCaseTest.java
@@ -4,20 +4,28 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.github.ricardocomar.testpyramid.microservice.PyramidMicroserviceApplication;
 import com.github.ricardocomar.testpyramid.microservice.book.model.Book;
 
-@RunWith(MockitoJUnitRunner.class)
-public class BookCreateUseCaseTest { 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {UseCaseConfiguration.class})
+@DirtiesContext
+@SpringBootTest(classes = PyramidMicroserviceApplication.class)
+@ActiveProfiles("usecase")
+public class BookCreateUseCaseTest {
 
-	@Mock
+	@Autowired
 	private BookCreateGateway createGateway;
 
-	@InjectMocks
+	@Autowired
 	private BookCreateUseCase useCase;
 
 	final Book book = Book.builder().name("John's thoughts").writter("John Snow").price(120.0).build();
